3cb0e3e2727eed464175be58177b40c0e1217f76
RizlH <rzlhrmwn00@gmail.com>
2025-06-20 11:39:25 +0700

    2. Membuat Form Pembayaran (dengan Input, Promo dan Pembayaran)

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+tailwind.config = {
+darkMode: 'class'
+}
+
 const toggleBtn = document.getElementById("darkModeToggle");
 const htmlElement = document.documentElement;
 
 toggleBtn.addEventListener("click", () => {
   htmlElement.classList.toggle("dark");
 });
+
